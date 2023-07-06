f06cabcad6aa2f1d3eb344848241f1621cdfe3bb
Suryansh Sharma <suryansh.sharma9315@gmail.com>
2023-07-06 16:26:09 +0530

    Updated Stack Screens

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const LoginScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>LoginScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- client/src/screens/OtpScreen.js
+++ client/src/screens/OtpScreen.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const OtpScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>OtpScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
